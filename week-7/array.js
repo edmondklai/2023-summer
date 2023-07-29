1d1ec5dba291dbaba6f6eb811a6e8b886b71e3c1
Edmond <edmondklai@gmail.com>
2023-07-29 11:41:47 -0500

    array concat and join

--- week-7/array.js
+++ week-7/array.js
@@ -46,3 +46,62 @@ function prependValue(ary, n) {
 
 console.log(prependValue(testAry, 10))
 
+
+function modifyArray(inputAry) {
+  inputAry.shift()
+  inputAry.unshift('JavaScript')
+  inputAry.pop()
+  inputAry.push('Programming')
+  return inputAry
+}
+
+console.log(
+  modifyArray(['purple', 3, true])
+)
+
+
+const arr1 = ['Java', 2, 3];
+const arr2 = [true, 5, 'Coffee'];
+const arr3 = arr2.concat(arr1)
+console.log(arr3.length)
+
+const arr4 = ['Jimmy', 'David', 'Haozhi', 'Ed']
+console.log(arr4.join(' '))
+
+// 1. mergeArrays
+function mergeArrays(ary1, ary2) {
+  return ary1.concat(ary2)
+}
+
+// 2
+function aryToString(ary1) {
+  return ary1.join(',')
+}
+
+function joinWithHyphen(ary1) {
+  return ary1.join('-')
+}
+
+// join and concat
+function joinAndConcat(ary1, ary2) {
+  const ary3 = ary1.concat(ary2)
+  return ary3.join(' ')
+}
+
+joinAndConcat(['a', 'b', 'c'], ['d', 'e'])
+
+function multiConcat(aryOfArys) {
+  let ary = []
+  for (let i = 0; i < aryOfArys.length; i = i + 1) {
+    ary = ary.concat(aryOfArys[i]);
+  }
+  return ary
+}
+
+console.log('multi', multiConcat([[1, 2], [3, 4]]))
+
+
+const aryOfArrays = [
+  [1, 3],
+  [3, 5]
+]
